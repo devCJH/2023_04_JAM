5e368c3dc451076c17ec0376195fb031d02d62ab
devCJH <axdsw121@gmail.com>
2023-05-03 19:51:10 +0900

    detail 기능 구현

--- src/com/KoreaIT/JAM/App.java
+++ src/com/KoreaIT/JAM/App.java
@@ -78,9 +78,32 @@ public class App {
 					for (Article article : articles) {
 						System.out.printf("%d	|	%s	|	%s\n", article.id, article.title, article.regDate);
 					}
+				} else if (cmd.startsWith("article detail ")) {
+					int id = Integer.parseInt(cmd.split(" ")[2]);
+
+					SecSql sql = new SecSql();
+					sql.append("SELECT *");
+					sql.append("FROM article");
+					sql.append("WHERE id = ?", id);
+					
+					Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
+					
+					if (articleMap.isEmpty()) {
+						System.out.printf("%d번 게시글은 존재하지 않습니다\n", id);
+						continue;
+					}
+					
+					Article article = new Article(articleMap);
+					
+					System.out.printf("== %d번 게시글 상세보기 ==\n", id);
+					System.out.printf("번호 : %d\n", article.id);
+					System.out.printf("작성날짜 : %s\n", article.regDate);
+					System.out.printf("수정날짜 : %s\n", article.updateDate);
+					System.out.printf("제목 : %s\n", article.title);
+					System.out.printf("내용 : %s\n", article.body);
+					
 				} else if (cmd.startsWith("article modify ")) {
 					int id = Integer.parseInt(cmd.split(" ")[2]);
-					System.out.printf("== %d번 게시글 수정 ==\n", id);
 
 					SecSql sql = SecSql.from("SELECT COUNT(*)");
 					sql.append("FROM article");
@@ -93,6 +116,7 @@ public class App {
 						continue;
 					}
 					
+					System.out.printf("== %d번 게시글 수정 ==\n", id);
 					System.out.printf("수정할 제목 : ");
 					String title = sc.nextLine();
 					System.out.printf("수정할 내용 : ");
