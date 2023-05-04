c7b575a0dc88596bab2481735e99d93adc5da2ca
devCJH <http-@naver.com>
2023-05-04 21:50:23 +0900

    MVC패턴으로 구조 변경중2

--- src/com/KoreaIT/JAM/App.java
+++ src/com/KoreaIT/JAM/App.java
@@ -3,11 +3,9 @@ package com.KoreaIT.JAM;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
+import com.KoreaIT.JAM.controller.ArticleController;
 import com.KoreaIT.JAM.controller.MemberController;
 import com.KoreaIT.JAM.util.DBUtil;
 import com.KoreaIT.JAM.util.SecSql;
@@ -27,6 +25,7 @@ public class App {
 			conn = DriverManager.getConnection(url, "root", "");
 
 			MemberController memberController = new MemberController(conn, sc);
+			ArticleController articleController = new ArticleController(conn, sc);
 			
 			while (true) {
 				System.out.printf("명령어) ");
@@ -40,73 +39,11 @@ public class App {
 				if (cmd.equals("member join")) {
 					memberController.doJoin();
 				} else if (cmd.equals("article write")) {
-					System.out.println("== 게시물 작성 ==");
-
-					System.out.printf("제목 : ");
-					String title = sc.nextLine();
-					System.out.printf("내용 : ");
-					String body = sc.nextLine();
-
-					SecSql sql = new SecSql();
-					sql.append("INSERT INTO article");
-					sql.append("SET regDate = NOW()");
-					sql.append(", updateDate = NOW()");
-					sql.append(", title = ?", title);
-					sql.append(", `body` = ?", body);
-					
-					int id = DBUtil.insert(conn, sql);
-					
-					System.out.printf("%d번 게시글이 생성되었습니다\n", id);
-
+					articleController.doWrite();
 				} else if (cmd.equals("article list")) {
-					List<Article> articles = new ArrayList<>();
-					
-					SecSql sql = new SecSql();
-					
-					sql.append("SELECT *");
-					sql.append("FROM article");
-					sql.append("ORDER BY id DESC");
-					
-					List<Map<String, Object>> articleListMap = DBUtil.selectRows(conn, sql);
-					
-					for(Map<String, Object> articleMap : articleListMap) {
-						articles.add(new Article(articleMap));
-					}
-
-					if (articles.size() == 0) {
-						System.out.println("존재하는 게시물이 없습니다");
-						continue;
-					}
-					System.out.println("== 게시물 리스트 ==");
-					System.out.println("번호	|	제목	|	날짜");
-
-					for (Article article : articles) {
-						System.out.printf("%d	|	%s	|	%s\n", article.id, article.title, article.regDate);
-					}
+					articleController.showList();
 				} else if (cmd.startsWith("article detail ")) {
-					int id = Integer.parseInt(cmd.split(" ")[2]);
-
-					SecSql sql = new SecSql();
-					sql.append("SELECT *");
-					sql.append("FROM article");
-					sql.append("WHERE id = ?", id);
-					
-					Map<String, Object> articleMap = DBUtil.selectRow(conn, sql);
-					
-					if (articleMap.isEmpty()) {
-						System.out.printf("%d번 게시글은 존재하지 않습니다\n", id);
-						continue;
-					}
-					
-					Article article = new Article(articleMap);
-					
-					System.out.printf("== %d번 게시글 상세보기 ==\n", id);
-					System.out.printf("번호 : %d\n", article.id);
-					System.out.printf("작성날짜 : %s\n", article.regDate);
-					System.out.printf("수정날짜 : %s\n", article.updateDate);
-					System.out.printf("제목 : %s\n", article.title);
-					System.out.printf("내용 : %s\n", article.body);
-					
+					articleController.showDetail(cmd);
 				} else if (cmd.startsWith("article modify ")) {
 					int id = Integer.parseInt(cmd.split(" ")[2]);
 
